24fbdc794921e2bd23bef96b27d896740029adea
Rafe <darkskies6969@gmail.com>
2025-07-15 16:17:54 +0300

    Main Commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           color: Colors.green[500],
